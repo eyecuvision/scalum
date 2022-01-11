f374b9e05f93235de1b489aa0beb3ce536ed22a0
Oguz Vuruskaner <ovuruska@outlook.com>
2022-01-11 13:50:17 +0300

    Added inverse function for scaling.

--- scalum/__init__.py
+++ scalum/__init__.py
@@ -1,6 +1,7 @@
-__version__ = "1.1.3"
+__version__ = "1.2.0"
 
 
 from .Scale import Scale
-from .ScaleWithYoloAnnotation import ScaleWithYoloAnnotation
 from .ScaleWithPolyAnnotation import ScaleWithPolyAnnotation
+from .ScaleWithPolyAnnotation import ScaleWithPolyAnnotation
+from .ScaleWithInverse import ScaleWithInverse
